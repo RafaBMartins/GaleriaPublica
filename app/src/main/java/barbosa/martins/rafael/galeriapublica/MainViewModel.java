a49e12f09fd7bd5e9758192a4d914f3b4a4e8615
20201tiimi0250 <rafaelmartinscouto@gmail.com>
2023-08-15 11:03:34 -0300

    11/08 começar o passo 8

--- app/src/main/java/barbosa/martins/rafael/galeriapublica/MainViewModel.java
+++ app/src/main/java/barbosa/martins/rafael/galeriapublica/MainViewModel.java
@@ -1,5 +1,7 @@
 package barbosa.martins.rafael.galeriapublica;
 
+import android.app.Application;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
@@ -9,5 +11,13 @@ public class MainViewModel extends AndroidViewModel {
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public int getNavigationOpSelected() {
+        return navigationOpSelected;
+    }
+
+    public void setNavigationOpSelected(int navigationOpSelected) {
+        this.navigationOpSelected = navigationOpSelected;
     }
 }
